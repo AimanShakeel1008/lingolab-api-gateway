2b3d69ef3a47161110ec9f8f5cd571a522fa7fe5
aimanshakeel <aimanshakeel1008@gmail.com>
2024-04-25 01:33:59 +0530

    initial setup

---
+++ src/main/java/com/lingolab/lingolabapigateway/LingolabApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lingolab.lingolabapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingolabApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingolabApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lingolab/lingolabapigateway/LingolabApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lingolab.lingolabapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingolabApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
